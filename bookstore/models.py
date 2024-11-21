2dee25e1b367f63447e645d9644a2f4b1dcbbd41
Hung <goihot85@gmail.com>
2024-11-21 15:39:15 +0700

    models

--- bookstore/models.py
+++ bookstore/models.py
@@ -35,6 +35,7 @@ class Customer(db.Model):
     address = Column(String(100))
 
     onlineOrders = relationship('OnlineOrder', backref='customer', lazy=True)
+    bills = relationship('Bill', backref='customer', lazy=True)
     
 
 class Staff(db.Model):
@@ -43,14 +44,38 @@ class Staff(db.Model):
     soDienThoai = Column(String(50))
     role_permision = Column(Enum(RolePermision))
 
+    # quan hệ one-to-many với bảng OnlineOrder
     onlineOrders = relationship('OnlineOrder', backref='staff', lazy=True)
+    # quan hệ one-to-many với bảng Bill
+    bills = relationship('Bill', backref='staff', lazy=True)
+    # quan hệ one-to-many với bảng PurchaseOrder
+    purchaseOrders = relationship('PurchaseOrder', backref='staff', lazy=True)
 
 
-class Order(db.Model):
+class Bill(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     ngayXuat = Column(Date, default=date.today())
 
+    customer_id = Column(Integer, ForeignKey(Customer.account_id), nullable=False)
+    staff_id = Column(Integer, ForeignKey(Staff.account_id), nullable=False)
+
     onlineOrder = relationship("OnlineOrder", backref="order", lazy=True, uselist=False)
+
+    books = relationship('BillDetail', backref='bill')
+
+
+class Author(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ten = Column(String(50), nullable=False)
+
+    books = relationship('Book', backref='author', lazy=True)
+
+
+class Type(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ten = Column(String(50), nullable=False)
+
+    books = relationship('Book', backref='type', lazy=True)
 
 
 class Book(db.Model):
@@ -60,10 +85,18 @@ class Book(db.Model):
 
     # quan hệ many-to-many với bảng OnlineOrder
     onlineOrders = relationship('OnlineOrderDetail', backref='book')
+    # quan hệ many-to-many với bảng Bill
+    bills = relationship('BillDetail', backref='book')
+    # quan hệ many-to-many với bảng PurchaseOrder
+    purchaseOrders = relationship('PurchaseOrderDetail', backref='book')
+    # quan hệ one-to-many với bảng Author
+    author_id = Column(Integer, ForeignKey(Author.id), nullable=False)
+    # quan hệ one-to-many với bảng Type
+    type_id = Column(Integer, ForeignKey(Type.id), nullable=False)
 
 
 class OnlineOrder(db.Model):
-    onlineOrder_id = Column(Integer, ForeignKey(Order.id), primary_key=True)
+    onlineOrder_id = Column(Integer, ForeignKey(Bill.id), primary_key=True)
     ngayTao = Column(Date, default=date.today(), nullable=False)
     ngayLayHang = Column(Date, default=date.today(), nullable=False)
     thanhToan = Column(Boolean, default=False)
@@ -76,11 +109,32 @@ class OnlineOrder(db.Model):
     books = relationship('OnlineOrderDetail', backref='onlineOrder')
 
 
+class PurchaseOrder(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ngayNhap = Column(Date, default=date.today())
+    staff_id = Column(Integer, ForeignKey(Staff.account_id), nullable=False)
+
+    books = relationship('PurchaseOrderDetail', backref='purchaseOrder')
+
+
 class OnlineOrderDetail(db.Model):
     book_id = Column(ForeignKey(Book.id), primary_key=True)
     onlineOrder_id = Column(ForeignKey(OnlineOrder.onlineOrder_id), primary_key=True)
     soLuong = Column(Integer, nullable=False)
     gia = Column(Integer, nullable=False)
+
+
+class BillDetail(db.Model):
+    bill_id = Column(ForeignKey(Bill.id), primary_key=True)
+    book_id = Column(ForeignKey(Book.id), primary_key=True)
+    soLuong = Column(Integer, nullable=False)
+    gia = Column(Integer, nullable=False)
+
+
+class PurchaseOrderDetail(db.Model):
+    purchaseOrder_id = Column(ForeignKey(PurchaseOrder.id), primary_key=True)
+    book_id = Column(ForeignKey(Book.id), primary_key=True)
+    soLuong = Column(Integer, nullable=False)
 
 
 if __name__ == '__main__':
