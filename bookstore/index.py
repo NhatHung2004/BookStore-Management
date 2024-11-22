b362dae50d9ef02ed326a2f6950877b10c05e5b2
Hung <goihot85@gmail.com>
2024-11-22 13:51:33 +0700

    update_frontend

--- bookstore/index.py
+++ bookstore/index.py
@@ -2,13 +2,17 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask import render_template, Flask
+from flask import render_template
 from bookstore import app
 
 
 @app.route("/")
 def index():
     return "Hello World"
+
+@app.route('/login')
+def login_process():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
