cc64055c8b4ee3688126f3305aa66f23eb2b3c08
Nhật Hưng <hung2004py@gmail.com>
2024-11-27 13:50:52 +0700

    Update __init__.py

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -8,7 +8,6 @@ import cloudinary
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://NhatHung:nhathung123@localhost/storedb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.secret_key = 'HGHJAHA^&^&*AJAVAHJ*^&^&*%&*^GAFGFAG'
 
 db = SQLAlchemy(app)
 login = LoginManager(app)
