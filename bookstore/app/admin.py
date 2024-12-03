e2d81878b88f4ca3fa5153f5637404fd55e36733
Hung <hung2004py@gmail.com>
2024-12-03 22:37:59 +0700

    hien thi san pham

--- bookstore/app/admin.py
+++ bookstore/app/admin.py
@@ -1,7 +1,6 @@
 from flask_admin.contrib.sqla import ModelView
 from app import db, admin
 from models import Staff, Book, Type, Author
-from wtforms import SelectField
 
 
 admin.add_view(ModelView(Book, db.session))
