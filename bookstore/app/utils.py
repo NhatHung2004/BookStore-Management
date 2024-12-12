d9d90aa299b8682e1ce0c2bf281f82efb6ca7fa1
Nhật Hưng <hung2004py@gmail.com>
2024-12-12 22:36:04 +0700

    gui mail va cap nhat ui (#3)

--- bookstore/app/utils.py
+++ bookstore/app/utils.py
@@ -14,3 +14,12 @@ def stats_cart(cart):
         "total_amount": total_amount,
         "total_quantity": total_quantity
     }
+
+
+def total_price(detailOrder):
+    total = 0
+
+    for do in detailOrder:
+        total += do['quantity'] * do['price']
+
+    return total
