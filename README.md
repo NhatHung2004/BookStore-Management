4ff6916fbd459d5ec8ad9dcef5cf815159bc75f1
NhatHung <hung2004py@gmail.com>
2025-08-08 11:15:28 +0700

    update README

--- README.md
+++ README.md
@@ -1,10 +1,75 @@
-# BookStore-Management
-Flask Python with MySQL
+# Quản lý tiệm sách
 
-Xử lý lỗi "module not found"
+Đây là website được xây dựng bằng Flask, sử dụng SQLAlchemy để tương tác với cơ sở dữ liệu và Flask-Admin để tạo giao diện admin. Dự án cũng tích hợp Flask-Login để xử lý xác thực người dùng và Flask-Mail để gửi email.
+## Các tính năng nổi bật
 
-import sys
+- Cơ sở dữ liệu: Sử dụng SQLAlchemy làm ORM (Object-Relational Mapper) để tương tác với cơ sở dữ liệu MySQL (PyMySQL).
+- Tác vụ nền: Sử dụng APScheduler để lên lịch và thực hiện các tác vụ nền định kỳ (xoá các đơn hàng đến hạn).
+- Lưu trữ cloud: Tích hợp Cloudinary để quản lý và lưu trữ file trên cloud.
+- Gửi email: Hỗ trợ gửi email thông qua Flask-Mail.
 
-import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+## Cài đặt
+
+1.Môi trường ảo
+
+```bash
+  # Tạo môi trường ảo
+  cd app
+  python -m venv venv
+
+  # Kích hoạt môi trường ảo
+  # Trên Windows
+  venv\Scripts\activate
+  # Trên macOS/Linux
+  source venv/bin/activate
+```
+
+2.Cài đặt các thư viện
+```bash
+  pip install -r requirements.txt
+```
+
+3.Đăng ký Cloudinary và VNPay để lưu trữ ảnh và test chức năng thanh toán
+
+```bash
+  # Cloudinary
+  https://cloudinary.com/users/register_free
+  # Đăng ký tài khoản vnpay developer kiểm thử
+  https://sandbox.vnpayment.vn/apis/docs/gioi-thieu/
+```
+
+4.Chạy dự án
+```bash
+  # Tạo dữ liệu mẫu
+  python models.py
+  # Chạy dự án
+  python routes.py
+```
+    
+## Các biến môi trường
+
+Để chạy dự án này, ta cần phải thêm các biến môi trường bên dưới vào file .env
+
+`API_KEY`
+
+`API_SECRET`
+
+`MYSQL_USERNAME`
+
+`MYSQL_PASSWORD`
+
+`CLOUD_NAME`
+
+`MAIL_USERNAME`
+
+`MAIL_PASSWORD`
+
+`SECRET_KEY`
+
+`VNP_TMN_CODE`
+
+`VNP_HASH_SECRET`
+
+`VNP_URL`
+
--- bookstore/app/__init__.py
+++ bookstore/app/__init__.py
@@ -4,11 +4,12 @@ from flask_login import LoginManager
 import cloudinary
 from vnpay import VNPay
 from flask_mail import Mail
+import os
 
 
 app = Flask(__name__)
 app.secret_key = 'HGHJAHA^&^&*AJAVAHJ*^&^&*%&*^GAFGFAG'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://NhatHung:nhathung123@localhost/storedb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.environ.get('MYSQL_USERNAME')}:{os.environ.get('MYSQL_PASSWORD')}@localhost/storedb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 8
 app.config["PAGE_MANAGE"] = 4
@@ -17,9 +18,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'goihot85@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ifevhqsigpuxoawa'
-app.config['MAIL_DEFAULT_SENDER'] = 'goihot85@gmail.com'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 
 
 db = SQLAlchemy(app)
@@ -28,15 +29,15 @@ mail = Mail(app)
 
 
 cloudinary.config(
-    cloud_name="dvahhupo0",
-    api_key="461556863519315",
-    api_secret="lBLWun6CURjcaUNB8G7qUQtHtxo",  # Click 'View API Keys' above to copy your API secret
+    cloud_name=os.environ.get('CLOUD_NAME'),
+    api_key=os.environ.get('API_KEY'),
+    api_secret=os.environ.get('API_SECRET'),
     secure=True
 )
 
 
-VNP_TMN_CODE = "265WA8JT"
-VNP_HASH_SECRET = "R98T5GT4US65NUVY1IIINX8LWN9BM2G2"
-VNP_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"
+VNP_TMN_CODE = os.environ.get('VNP_TMN_CODE')
+VNP_HASH_SECRET = os.environ.get('VNP_HASH_SECRET')
+VNP_URL = os.environ.get('VNP_URL')
 
 vnpay = VNPay(VNP_TMN_CODE, VNP_HASH_SECRET, VNP_URL)
